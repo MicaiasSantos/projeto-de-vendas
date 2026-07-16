265cf694852498b5c87fcbae06dc6bc46c5f383d
MicaiasSantos <micaiasg3@gmail.com>
2026-07-16 01:03:54 -0300

    corrigindo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function comprar() {
+function comprar(produtoSelecionado) {
     const mensagem =`Olá, quero comprar o produto ${produtoSelecionado} `;
      const url = `https://wa.me/5585987088527?text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
